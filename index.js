e67446247d330d5bba66f8fc52bd7f80c2e2c3ad
Gavrilov Ruslan <miditab@yandex.ru>
2017-10-24 20:13:16 +0400

    fix

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ console.log("Wait...")
 console.time("lead time")
 const result = compNames.reduce( (accumulator, currentValue) => {
   const file2 = accumulator.replace(/(<Field)([ \n][ \S\n]*?label[ \S\n]*?>)([ \S\n]*?)(<[ \S\n]*?\/Field)(>)/gu, "$1UI $2 $4UI$5")
-  let file3 = file2.replace(new RegExp(`<(${currentValue})([ \\S\\n]*?)\\/>`, "gu"), `<Field component=\{${currentValue}\} props={{$2}}/>`)
+  let file3 = file2.replace(new RegExp(`<(${currentValue})([ \\S\\n]*?)>([ \\S\\n]*?)<\/${currentValue}>`, "gu"), `<Field component=\{${currentValue}\} props={{$2}}>$3</Field>`)
+  file3 = file3.replace(new RegExp(`<(${currentValue})([ \\S\\n]*?)\\/>`, "gu"), `<Field component=\{${currentValue}\} props={{$2}}/>`)
   for(let i = 20; i > 0; i --){
     file3 = file3.replace(/(<Field component=\{.+\} props=\{\{)([ \n]*[a-zA-Z0-9]*)=\{([(),:\[\] "\-.a-zA-Z0-9А-Яа-я]*)\}([ \n]*[ \n\S]*?)(\}\}[ \S\n]*?\/>)/gu, "$1$4 $2: $3, $5")
     file3 = file3.replace(/(<Field component=\{.+\} props=\{\{)([ \n]*[a-zA-Z0-9]*)="([(),:\[\] "\-.a-zA-Z0-9А-Яа-я]*)"([ \n]*[ \n\S]*?)(\}\}[ \S\n]*?\/>)/gu, "$1$4 $2: '$3', $5")
